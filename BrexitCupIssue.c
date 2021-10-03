68d88d8b9fbe33eae90fd050055eb1a465d86b26
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-03 14:01:22 +0300

    Update BrexitCupIssue.c
    Added #define after provided feedback

--- BrexitCupIssue.c
+++ BrexitCupIssue.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+#define OUNCE_TO_CUP_RATIO 8
+
 int main() {
 
     int iOuncesOrdered = 0, iConvertedOuncesToCups = 0;
     printf ("How many ounces would you like to order:\n");
     scanf ("%d", &iOuncesOrdered);
 
-    iConvertedOuncesToCups = iOuncesOrdered / 8;
+    iConvertedOuncesToCups = iOuncesOrdered / OUNCE_TO_CUP_RATIO;
     printf("\nFor %d ounces you would get %d cups.", iOuncesOrdered, iConvertedOuncesToCups);
 
     return 0;
