ab7909f0f27485ddab299df27293e5fb76fd4011
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-03 14:03:02 +0300

    Update EarthToJupiter.c
    Added #define after provided feedback

--- EarthToJupiter.c
+++ EarthToJupiter.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define EARTH_DAYS_TO_JUPITER_YEARS 4383
+
 int main() {
     unsigned int iDaysOnEarth = 0;
     double dConvertedEarthDaysToJupiterYears = 0.0;
@@ -7,7 +9,7 @@ int main() {
     printf ("How many days on Earth would you like to convert to Jupiter years?\n");
     scanf ("%d", &iDaysOnEarth);
 
-    dConvertedEarthDaysToJupiterYears =  iDaysOnEarth / 4383;
+    dConvertedEarthDaysToJupiterYears =  iDaysOnEarth / EARTH_DAYS_TO_JUPITER_YEARS;
     printf("%d days on Earth are equal to %lf years on Jupiter.", iDaysOnEarth, dConvertedEarthDaysToJupiterYears);
 
     return 0;
