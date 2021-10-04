11f68e2d51bdaada0d078c81267f63daed19d557
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-04 14:59:48 +0300

    Create printValues2.c

---
+++ printValues2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    short int a = 24212;
+    int b = -1357674;
+    unsigned int c = 1357674;
+    int d = -1357674000;
+    unsigned int e = 3657895000;
+    
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", d);
+    printf("%u\n", e);
+
+    return 0;
+}
