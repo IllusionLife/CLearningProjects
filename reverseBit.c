f4640952b19fe4458fa7fe99e35d6bb7f75e8c8b
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-05 15:02:21 +0300

    Create reverseBit.c

---
+++ reverseBit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int reverseBit(int num, int i) {
+    if ((num >> i) & 1 == 1) {
+        return (num & ~(1 << i));
+    }
+    else {
+        return (num | (1 << i));
+    };
+}
+
+int main() {
+    int input = 0;
+    int index = 0;
+    printf("Please enter your char/number:\n");
+    scanf("%d", &input);
+    printf("Please enter the index of the byte you wish to reverse:\n");
+    scanf("%d", &index);
+    printf("The resulted byte is %d", reverseBit(input, index));
+    return 0;
+}
