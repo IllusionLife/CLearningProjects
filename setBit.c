da8e3963f32e25c7a79f483f32062afbf73d8aa9
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-05 14:54:16 +0300

    Create setBit.c

---
+++ setBit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int setBit(int num, int i) {
+    return (num | (1 << i));
+}
+
+int main() {
+    int input = 0;
+    int index = 0;
+    printf("Please enter your char/number:\n");
+    scanf("%d", &input);
+    printf("Please enter the index of the byte you wish to change to 1:\n");
+    scanf("%d", &index);
+    printf("The resulted byte is %d", setBit(input, index));
+    return 0;
+}
