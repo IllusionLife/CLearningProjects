f061d9497f9e291cbb03d772f9a6e196ea85a1d4
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-05 14:46:15 +0300

    Create bitAt.c

---
+++ bitAt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int bitAt(int num, int i){
+    return (num >> i) & 1 ? 1 : 0;
+}
+
+int main() {
+    int input = 0;
+    int index = 0;
+    printf("Please enter your char/number:\n");
+    scanf("%d", &input);
+    printf("Please enter the index of the byte you wish to check:\n");
+    scanf("%d", &index);
+    printf("The index at position %d is %d", index, bitAt(input, index));
+    return 0;
+}
