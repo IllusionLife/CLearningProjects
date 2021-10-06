2a9c50c7dd552ff67ff836b624d5129b1fc0b06d
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-06 14:40:23 +0300

    Create print0xAA.c

---
+++ print0xAA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+char test[] = {0xAA, 0xAA, 0xAA, 0xAA};
+
+    printf("Char: %c\n", test[0]);
+    printf("float: %f\n", test[1]);
+    printf("Int: %d\n", test[2]);
+    printf("Unsigned: %u\n", test[3]);
+    return 0;
+}
