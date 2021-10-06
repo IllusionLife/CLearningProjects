27e74a0336312f1899a1069ae0e977d39e707c6d
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-07 02:40:27 +0300

    Update circleArea.c

--- circleArea.c
+++ circleArea.c
@@ -2,15 +2,45 @@
 
 const double pi = 3.14159265359;
 
-double circleArea(float radius) {
-    return radius * radius * pi;
+void circleArea() {
+    float radius = 0.0;
+    printf("\nEnter the radius of your circle: ");
+    scanf("%f", &radius);
+    printf("The area of a circle with a radius of %.2f is: %f\n", radius , (radius*radius*pi));
+}
+
+double ellipseArea() {
+    float sideA = 0.0, sideB = 0.0;
+    printf("\nEnter side A: ");
+    scanf("%f", &sideA);
+    printf("Enter side B: ");
+    scanf("%f", &sideB);
+    printf("The area of an ellipse with sides  %.2f and %.2f is: %f\n", sideA, sideB , (sideA*sideB*pi));
 }
 
 int main() {
-    float rad = 0.0;
-    printf("Enter the radius of your circle: ");
-    scanf("%f", &rad);
-    printf("The area of a circle with a radius of %.2f is: %f", rad, circleArea(rad));
+
+    char selection = 0;
+    do {
+        printf("\nPlease select an operation:\n");
+        printf("1. Calculate circle area\n");
+        printf("2. Calculate ellipse area\n");
+        printf("0. Exit program\n");
+        scanf("%d", &selection);
+        if (selection == 1) {
+            circleArea();
+        }
+        else if (selection == 2) {
+            ellipseArea();
+        }
+        else {
+            break;
+        }
+        printf("\nWould you like to continue?\n");
+        printf("0. No\n");
+        printf("1. Yes\n");
+        scanf("%d", &selection);
+    } while (selection != 0);
 
     return 0;
 }
