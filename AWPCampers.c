44cd46890a1f53dd44b8d0de6ebf6c2580814e66
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-10-07 02:58:42 +0300

    Update AWPCampers.c

--- AWPCampers.c
+++ AWPCampers.c
@@ -2,57 +2,70 @@
 
 int main() {
     int campersAvailable = 3, caravansAvailable = 3, rentedCampers = 0, rentedCaravans = 0, rentDays = 0;
-    float priceCamper = 100.0, priceCaravan = 90.0, totalPrice = 0.0;
-    unsigned char select = 0;
+    float priceCamper = 100.0, priceCaravan = 90.0, currentPrice = 0.0, totalPrice = 0.0;
+    unsigned char select = 0, again = 0;;
     printf("Hello! Welcome to AWP Campers!\n");
-    printf("We currently have %d caravans and %d campers available\n", caravansAvailable, campersAvailable);
-    printf("Which vehicle are you interested in?\n1.Caravans\n2.Campers\n\nEnter your selection here: ");
-    scanf("%d", &select);
-    while(select != 1 && select != 2) {
-        printf("Invalid input. Please use a valid option.\n");
+    do {
+        printf("\nWe currently have %d caravans and %d campers available\n", caravansAvailable, campersAvailable);
         printf("Which vehicle are you interested in?\n1.Caravans\n2.Campers\n\nEnter your selection here: ");
         scanf("%d", &select);
-    }
-    if (select == 1) {
-        printf("\nGreat choice! How many caravans would you like to rent?\nReminder, we have %d caravans available.\n", caravansAvailable);
-        scanf("%d", &rentedCaravans);
-        while ((rentedCaravans > caravansAvailable) || (rentedCaravans < 0))
-        {
+        while(select != 1 && select != 2) {
             printf("\nInvalid input. Please use a valid option.\n");
-            printf("How many caravans would you like to rent?\nReminder, we have %d caravans available.\n", caravansAvailable);
+            printf("Which vehicle are you interested in?\n1.Caravans\n2.Campers\n\nEnter your selection here: ");
+            scanf("%d", &select);
+        }
+        if (select == 1) {
+            printf("\nGreat choice! How many caravans would you like to rent?\nReminder, we have %d caravans available.\n", caravansAvailable);
             scanf("%d", &rentedCaravans);
-        }
-        printf("\nSounds like a great trip. And for how many days would you like to have them rented?\n");
-        scanf("%d", &rentDays);
-        while (rentDays < 0)
-        {
-            printf("\nInvalid input. Please use a valid option.\n");
-            printf("Sounds like a great trip. And for how many days would you like to have them rented?\n");
+            while ((rentedCaravans > caravansAvailable) || (rentedCaravans < 0))
+            {
+                printf("\nInvalid input. Please use a valid option.\n");
+                printf("How many caravans would you like to rent?\nReminder, we have %d caravans available.\n", caravansAvailable);
+                scanf("%d", &rentedCaravans);
+            }
+            printf("\nSounds like a great trip. And for how many days would you like to have them rented?\n");
             scanf("%d", &rentDays);
+            while (rentDays < 0)
+            {
+                printf("\nInvalid input. Please use a valid option.\n");
+                printf("Sounds like a great trip. And for how many days would you like to have them rented?\n");
+                scanf("%d", &rentDays);
+            }
+            printf("\nGreat! So you wish to rent %d caravans (each costing %.2fBGN per day) for %d days.\n", rentedCaravans, priceCaravan, rentDays);
+            currentPrice = rentedCaravans * priceCaravan * rentDays;
+            printf("This price for this rent is: %.2f", currentPrice);
+            totalPrice += currentPrice;
+            caravansAvailable -= rentedCaravans;
         }
-        printf("\nGreat! So you wish to rent %d caravans (each costing %.2fBGN per day) for %d days.\n", caravansAvailable, priceCaravan, rentDays);
-        printf("In total, this will cost you %.2f BGN.\n", (caravansAvailable * priceCaravan * rentDays));
-    }
-    else {
-        printf("Great choice! How many campers would you like to rent?\nReminder, we have %d campers available.\n", campersAvailable);
-        scanf("%d", &rentedCampers);
-        while ((rentedCampers > campersAvailable) || (rentedCampers < 0))
-        {
-            printf("Invalid input. Please use a valid option.\n");
-            printf("How many campers would you like to rent?\nReminder, we have %d campers available.\n", campersAvailable);
+        else {
+            printf("\nGreat choice! How many campers would you like to rent?\nReminder, we have %d campers available.\n", campersAvailable);
             scanf("%d", &rentedCampers);
-        }
-        printf("Sounds like a great trip. And for how many days would you like to have them rented?\n");
-        scanf("%d", &rentDays);
-        while (rentDays < 0)
-        {
-            printf("Invalid input. Please use a valid option.\n");
-            printf("Sounds like a great trip. And for how many days would you like to have them rented?\n");
+            while ((rentedCampers > campersAvailable) || (rentedCampers < 0))
+            {
+                printf("\nInvalid input. Please use a valid option.\n");
+                printf("How many campers would you like to rent?\nReminder, we have %d campers available.\n", campersAvailable);
+                scanf("%d", &rentedCampers);
+            }
+            printf("\nSounds like a great trip. And for how many days would you like to have them rented?\n");
             scanf("%d", &rentDays);
+            while (rentDays < 0)
+            {
+                printf("\nInvalid input. Please use a valid option.\n");
+                printf("Sounds like a great trip. And for how many days would you like to have them rented?\n");
+                scanf("%d", &rentDays);
+            }
+            printf("\nGreat! So you wish to rent %d campers (each costing %.2fBGN per day) for %d days.\n", rentedCampers, priceCamper, rentDays);
+            currentPrice = rentedCampers * priceCamper * rentDays;
+            printf("This price for this rent is: %.2f", currentPrice);
+            totalPrice += currentPrice;
+            campersAvailable -= rentedCampers;
         }
-        printf("Great! So you wish to rent %d campers (each costing %.2fBGN per day) for %d days.\n", campersAvailable, priceCamper, rentDays);
-        printf("In total, this will cost you %.2f BGN.\n", (campersAvailable * priceCamper * rentDays));
-    }
+        printf("\nWould you like to rent another vehicle?\n");
+        printf("0. No\n");
+        printf("1. Yes");
+        scanf("%d", &again);
+    } while (again != 0);
+    printf("In total, this will cost you %.2f BGN.\n", totalPrice);
 
     return 0;
 }
